384244c1ac14d7a827ae693f0c35effac39ae961
victor hazbun <victorhazbun87@gmail.com>
2019-02-09 19:25:01 +1100

    Delete a Document

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -37,7 +37,18 @@ export const updateOne = model => async (req, res) => {
   res.status(200).json({ data: doc })
 }
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
+
+  const doc = await model.findOneAndRemove({ _id: id, createdBy: userId }).exec()
+
+  if (!doc) {
+    return res.status(404).end()
+  }
+
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
